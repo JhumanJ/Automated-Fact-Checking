5e8831c185db4d3e8389ba75d3eb17a368309646
JhumanJ <julien@nahum.net>
2019-04-03 22:28:15 +0200

    Compute top5 for each claim

--- q4.py
+++ q4.py
@@ -14,19 +14,15 @@ To speed up computations pre-filters documents to keep only relevant ones.
 
 To do so, we use the inverted index
 """
-def findRelevantDocumentsClaims(nbClaims=10000):
+def findRelevantDocumentsClaims(claims):
 
-    relevantDocsPath = cache_path + "relevantDocs"+str(nbClaims)+"Claims.json"
+    relevantDocsPath = cache_path + "relevantDocs"+str(len(claims))+"Claims.json"
     docInvertedIndexFile = output_path + 'docInvertedIndex.json'
 
     # If already computed just return it
     if os.path.isfile(relevantDocsPath):
         print("Computations already done. Loading results from: {}", relevantDocsPath)
         return openJsonDict(relevantDocsPath)
-
-    # First load claims
-    claims = load_dataset_json(train_path)[0:nbClaims]
-    print("{} claims loaded,".format(nbClaims))
 
     # Loading inverted index
     print("Loading inverted index.")
@@ -64,9 +60,66 @@ def findRelevantDocumentsClaims(nbClaims=10000):
     saveDictToJson(relevantDocs,relevantDocsPath)
     return relevantDocs
 
-def question4():
+def computeTop5dirichlet(claims,relevantDocs):
+    claimsTop5Path = cache_path + "claims10000Top5.json"
 
-    findRelevantDocumentsClaims()
+    # If already computed just return it
+    if os.path.isfile(claimsTop5Path):
+        print("Computations already done. Loading results from: {}", claimsTop5Path)
+        return openJsonDict(claimsTop5Path)
+
+    # load wiki
+    wikiArticles = parse_wiki(wiki_pages_path, wiki_parsed_cache_path)
+
+    # Compute vocabulary size and collection frequency (using result from q1)
+    wordsDictionnary = getTextStatistics()
+    vocSize = len(wordsDictionnary.keys())
+    collectionFrequency = sum(wordsDictionnary.values())
+    avgWordPerDocument = float(collectionFrequency) / len(wikiArticles)
+    print(
+        "Vocabulary size: {}, collection frequency: {}, avg word per document: {}".format(vocSize, collectionFrequency,
+                                                                                          avgWordPerDocument))
+
+    claimsTop5 = {}
+    for claim in tqdm(claims):
+        claimWords = removeStopWords(splitWords(claim['claim']))
+        top5 = []
+
+        # Find best documents
+        for docId in relevantDocs[claim]:
+            doc = wikiArticles[doc]
+            words = splitWords(doc)
+
+            # Compute model and score for document using dirichlet and cosine sim.
+            model = computeDirichletQueryLikelihoodModel(words, claimWords, wordsDictionnary, collectionFrequency, avgWordPerDocument)
+            score = computeQueryScore(model, claimWords)
+
+            # Use heap to keep the top 5 of each claim
+            if len(top5) < 5:
+                heappush(top5, (score, docId))
+            else:
+                heappushpop(top5, (score, docId))
+
+        claimsTop5[claim['id']] = top5
+
+    del wikiArticles
+    del wordsDictionnary
+
+    saveDictToJson(claimsTop5, claimsTop5Path)
+
+    return claimsTop5
+
+
+
+def question4(nbClaims = 10000):
+
+    # First load claims
+    claims = load_dataset_json(train_path)[0:nbClaims]
+    print("{} claims loaded,".format(nbClaims))
+
+    relevantDocs = findRelevantDocumentsClaims(claims)
+    top5Docs = computeTop5dirichlet(claims,relevantDocs)
+
     return
 
     # TODO: remove return above. below is for later when computing idf Below is
