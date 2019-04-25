60497c00ab576db5c571b82718d059384ac156e3
Julien Nahum <julien@nahum.net>
2019-04-25 13:25:47 +0000

    final code

--- q6.py
+++ q6.py
@@ -3,11 +3,16 @@ from lib.utils import *
 
 from paths import *
 from tqdm import tqdm
+from sklearn.preprocessing import minmax_scale
+from sklearn.preprocessing import normalize
+from sklearn.utils import shuffle
+
 
 import os, json, gc, time, random
 import pandas as pd
+import numpy as np
 
-from q4_q5 import loadGoogleWord2Vec
+from q4_q5 import loadGoogleWord2Vec, relevanceEvaluation
 
 # Import for lstm text classification
 from sklearn.model_selection import train_test_split
@@ -19,6 +24,12 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing import sequence
 from keras.utils import to_categorical
 from keras.callbacks import EarlyStopping
+
+
+embeddedTrainingTextDataPath = cache_path + 'embeddedTrainingTextDataPath.json'
+embeddedTestingTextDataPath = cache_path + 'embeddedTestingTextDataPath.json'
+trainingTextDataPath = cache_path + 'trainingTextDataPath.json'
+testingTextDataPath = cache_path + 'testingTextDataPath.json'
 
 
 def arrayToSentenceData(claims):
@@ -46,14 +57,11 @@ def arrayToSentenceData(claims):
 
     return dataset
 
-def claimEmbedding(claim):
+def claimEmbedding(claim,model):
 
     """
         Again use wordembedding of google to transform a claim into a matrix
     """
-
-    # Load google word2vector trained dataset
-    model = loadGoogleWord2Vec()
 
     claim = removeStopWords(splitWords(claim))
     count = 0
@@ -64,16 +72,16 @@ def claimEmbedding(claim):
             # Sum all dimensions
             for index, value in enumerate(model[word]):
                 sentence[index] += value
-        # Finally average everything
-        for index, value in enumerate(sentence):
-            sentence[index] = sentence[index] / float(count)
+    # Finally average everything
+    for index, value in enumerate(sentence):
+        sentence[index] = sentence[index] / float(count)
 
     return sentence
 
-def getRNN(inputLenght,maxWords):
+def getLSTM(inputLenght,batch_size):
     inputs = Input(name='inputs',shape=[inputLenght])
-    # layer = Embedding(maxWords,50,input_length=inputLenght)(inputs)
-    layer = LSTM(64)(layer)
+    layer = Embedding(batch_size,50,input_length=inputLenght)(inputs)
+    layer = LSTM(64,name='FactCheckingLstm')(layer)
     layer = Dense(256,name='FC1')(layer)
     layer = Activation('relu')(layer)
     layer = Dropout(0.5)(layer)
@@ -82,10 +90,27 @@ def getRNN(inputLenght,maxWords):
     model = Model(inputs=inputs,outputs=layer)
     return model
 
-def question6():
+def getEmbeddedDatasets():
 
-    trainingTextDataPath = cache_path + 'trainingTextDataPath.json'
-    testingTextDataPath = cache_path + 'testingTextDataPath.json'
+    # First check if embedded dataset are already prepared =======
+    dataset = None
+    testDataset = None
+
+    # Training set computation
+    if os.path.isfile(embeddedTrainingTextDataPath):
+        print("Computations already done. Loading results from: ", embeddedTrainingTextDataPath)
+        dataset = openJsonDict(embeddedTrainingTextDataPath)
+
+
+    # Testing set computation
+    if os.path.isfile(embeddedTestingTextDataPath):
+        print("Computations already done. Loading results from: ", embeddedTestingTextDataPath)
+        testDataset = openJsonDict(embeddedTestingTextDataPath)
+
+    if not dataset is None and not testDataset is None:
+        return dataset, testDataset
+
+    # If embedded dataset are not done yet ============
 
     # Training set computation
     if os.path.isfile(trainingTextDataPath):
@@ -107,61 +132,165 @@ def question6():
         testDataset = arrayToSentenceData(claims)
         saveDictToJson({'data':testDataset},testingTextDataPath)
 
+    # Load google word2vector trained dataset
+    model = loadGoogleWord2Vec()
     # Apply word embedding to training data using google trained word2vec
-    temp = []
-    for claim in dataset['data']:
-        temp.append(claimEmbedding(claim))
-    dataset['data'] = temp
+    temp = {
+    'data': [],
+    'label': []
+    }
+    for claim in tqdm(dataset):
+        temp['data'].append(claimEmbedding(claim[1],model))
+        temp['label'].append(claim[0])
+    dataset = temp
 
     # Apply word embedding to test data using google trained word2vec
-    temp = []
-    for claim in testDataset['data']:
-        temp.append(claimEmbedding(claim))
-    testDataset['data'] = temp
+    temp = {
+    'data': [],
+    'label': []
+    }
+    for claim in tqdm(testDataset):
+        temp['data'].append(claimEmbedding(claim[1],model))
+        temp['label'].append(claim[0])
+    testDataset = temp
+
+    saveDictToJson(dataset,embeddedTrainingTextDataPath)
+    saveDictToJson(testDataset,embeddedTestingTextDataPath)
+
+    return dataset, testDataset
 
 
-    # supportCount = len(dataset[dataset['label']==1])
-    # print("Training: {} labels SUPPORTS and {} label REFUTES".format(supportCount, len(dataset)-supportCount))
-    # supportCount = len(testDataset[testDataset['label']==1])
-    # print("Testing: {} labels SUPPORTS and {} label REFUTES".format(supportCount, len(testDataset)-supportCount))
+def question6(use_word2vec=False):
 
-    # build datasets
-    X_train = dataset['data']
-    Y_train = dataset['label']
-    X_test = testDataset['data']
-    Y_test = testDataset['label']
+    if use_word2vec:
+        dataset, testDataset = getEmbeddedDatasets()
 
-    # Word embedding settings (300 dimensions in google word2vec)
-    max_len = 300
-    max_words = 1000
+        # build datasets
+        X_train = np.array(dataset['data'])
+        Y_train = np.array(dataset['label'])
+        X_train, Y_train = shuffle(X_train, Y_train, random_state=0)
 
+        X_test = np.array(testDataset['data'])
+        Y_test = np.array(testDataset['label'])
 
-    # tok = Tokenizer(num_words=max_words)
-    #
-    # tok.fit_on_texts(X_train)
-    # sequences = tok.texts_to_sequences(X_train)
-    # sequences_matrix = sequence.pad_sequences(sequences,maxlen=max_len)
-    # print(X_train.head())
+        supportCount = sum(Y_train)
+        print("Training ({}): {} labels SUPPORTS and {} label REFUTES".format(len(X_train),supportCount, len(X_train)-supportCount))
+        supportCount = sum(Y_test)
+        print("Testing ({}): {} labels SUPPORTS and {} label REFUTES".format(len(X_test), supportCount, len(X_test)-supportCount))
+
+        # Nomrmalize the dataset
+        for index in range(len(X_train)):
+            X_train[index] = minmax_scale(X_train[index])
+        for index in range(len(X_test)):
+            X_test[index] = minmax_scale(X_test[index])
+
+        max_len = 300
+        training_zise = 1000
+
+    else:
+
+        # Use keras tokenizer which better represents the sentences
+
+        # Training set computation
+        if os.path.isfile(trainingTextDataPath):
+            print("Computations already done. Loading results from: ", trainingTextDataPath)
+            dict = openJsonDict(trainingTextDataPath)
+            dataset = dict["data"]
+        else:
+            claims = load_dataset_json(train_path)
+            dataset = arrayToSentenceData(claims)
+            saveDictToJson({'data':dataset},trainingTextDataPath)
+
+        # Testing set computation
+        if os.path.isfile(testingTextDataPath):
+            print("Computations already done. Loading results from: ", testingTextDataPath)
+            dict = openJsonDict(testingTextDataPath)
+            testDataset = dict["data"]
+        else:
+            claims = load_dataset_json(labeled_development_path)
+            testDataset = arrayToSentenceData(claims)
+            saveDictToJson({'data':testDataset},testingTextDataPath)
+
+        max_len = 150
+        training_zise = 1000
+
+        dataset = pd.DataFrame(dataset,columns=['label','data'])
+        testDataset = pd.DataFrame(testDataset,columns=['label','data'])
+
+        # build datasets
+        X_train = dataset['data']
+        Y_train = dataset['label'].values
+        X_test = testDataset['data']
+        Y_test = testDataset['label'].values
+
+        # Tokenize the sentences
+        tok = Tokenizer(num_words=training_zise)
+
+        tok.fit_on_texts(X_train)
+        sequences = tok.texts_to_sequences(X_train)
+        X_train = sequence.pad_sequences(sequences,maxlen=max_len)
+        test_sequences = tok.texts_to_sequences(X_test)
+        X_test = sequence.pad_sequences(test_sequences,maxlen=max_len)
+
+        X_train, Y_train = shuffle(X_train, Y_train, random_state=0)
 
     # Build network
-    model = getRNN(max_len,max_words)
+    model = getLSTM(max_len,training_zise)
     model.summary()
     model.compile(loss='binary_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
 
     # Train network
     # todo: increase number of epochs
-    model.fit(X_train,Y_train,batch_size=128,epochs=1,validation_split=0.2,callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)])
+    print("Starting to train")
+    model.fit(X_train,Y_train,batch_size=128,epochs=5,verbose=1,validation_split=0.2,callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)])
 
-    # test
-    test_sequences = tok.texts_to_sequences(X_test)
-    test_sequences_matrix = sequence.pad_sequences(test_sequences,maxlen=max_len)
-    accr = model.evaluate(test_sequences_matrix,Y_test)
-    results = model.predict(test_sequences_matrix)
-    print(results)
-    print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
+    # testing model
+    results = model.predict(X_test)
+
+    actual_results = [0 if result <0.5 else 1 for result in results]
+    comparison = list(zip(actual_results,Y_test))
+    print(relevanceEvaluation(comparison))
+
+    # Genreate output File
+    testClaims = load_dataset_json(labeled_development_path)
+    filteredTestClaims = []
+
+    wikiArticlesLines = parse_wiki_lines(wiki_pages_path, wiki_parsed_lines_cache_path)
+    for claim in tqdm(testClaims):
+
+        if claim['label'] in ['SUPPORTS','REFUTES']:
+            claimEvidenceSentences = []
+
+            # Find sentences
+            for evidenceGroup in claim['evidence']:
+                for evidence in evidenceGroup:
+                    if evidence[2]!= '' and not evidence[2] is None and evidence[2] in wikiArticlesLines:
+                        claimEvidenceSentences.append(wikiArticlesLines[evidence[2]][str(evidence[3])])
+
+            # build dataset
+            if len(claimEvidenceSentences)> 0:
+                filteredTestClaims.append(claim)
+
+
+
+    with open(output_path+'predictions.jsonl', 'w') as outfile:
+
+        for idx, claim in tqdm(enumerate(filteredTestClaims)):
+            if claim['label'] in ['SUPPORTS','REFUTES']:
+
+                jsonDict = {
+                    "id": claim['id'],
+                    "predicted_label": ('SUPPORTS' if actual_results[idx] == 1 else 'REFUTES'),
+                    "predicted_evidence": []
+                }
+
+                json.dump(jsonDict, outfile)
+                outfile.write('\n')
+
 
     # Test set
-    # Loss: 0.633  Accuracy: 0.628
+    # {'accuracy': 0.6246881850480006, 'precision': 0.5747553461399058, 'recall': 0.9588813303099017, 'f1': 0.718712820803354}
+
 
 
 question6()
